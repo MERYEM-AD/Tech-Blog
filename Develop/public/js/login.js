39b236012d1dcdbc5551752b7ff639e9b154775f
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-12-29 18:49:52 -0500

    Fix session for log in

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -2,20 +2,20 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // login for existing user
-  const email = document.querySelector('#email-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (username && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert('Sorry , Failed to log in.');
+      alert('Sorry  login in , Failed to log in.');
     }
   }
 };
@@ -25,20 +25,19 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
+  if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/');
     } else {
-      alert(' Sorry ,Failed to sign up.');
+      alert(' Sorry , ing up Failed to sign up.');
     }
   }
 };
